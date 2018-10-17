3e434857904ff3cc2df71c7da829af69652eaae3
sachikz <sachikz01@gmail.com>
2018-10-17 03:20:52 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,19 @@ User.create!(name: "Example User",
               activated_at: Time.zone.now)
 end
 
+#マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+#リレーションシップ
+#最初のユーザーが、ユーザー3～51までフォロー
+#ユーザー4～41が、最初のユーザーをフォロー
+users = User.all
+user = users.first
+following = users[2..50] #配列は0から始まるからね
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -84,4 +84,35 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+  
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer) # MはAをフォローしてないよね？
+    michael.follow(archer)                # MはAをフォローした！
+    assert michael.following?(archer)     # MはAをフォローしてるよね？
+    assert archer.followers.include?(michael) #AのフォロワーにMが含まれてるよね？
+    michael.unfollow(archer)              # MはAのフォローをやめた！
+    assert_not michael.following?(archer) # MはAをフォローしてないよね？
+  end
+  
+  test "feed should have the right posts" do
+    # MichaelはLanaをフォローしている
+    # MichaelはArcherをフォローしていない
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    #フォローしているユーザーの投稿を確認
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    #自分自身の投稿を確認
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    #フォローしていないユーザーの投稿を確認
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
